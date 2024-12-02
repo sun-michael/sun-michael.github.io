d3525cdb2492c08b0a5557938a3489ed3980866e
Michael Sun <sun.m710@gmail.com>
2024-12-01 21:52:18 -0500

    adding new colors

--- src/components/skill-list/skill-list.components.jsx
+++ src/components/skill-list/skill-list.components.jsx
@@ -1,8 +1,10 @@
+import "./skill-list.styles.scss";
+
 const SkillList = ({ skills }) => {
   return (
     <div className="skill-list-container">
       {skills.map((skill, index) => (
-        <div key={index} class="badge text-bg-secondary text-wrap">
+        <div key={index} class="badge text-wrap">
           {skill}
         </div>
       ))}
